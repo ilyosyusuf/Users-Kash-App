13013f0de005d052e42a0b7cd1d38247273b18a4
ilyosyusuf <ilyosyusufforweb@gmail.com>
2022-07-27 14:29:09 +0500

    home bloc was added, home view was started

--- lib/screens/bloc/home_bloc.dart
+++ lib/screens/bloc/home_bloc.dart
@@ -3,4 +3,14 @@ import 'package:users/repositories/user_repository.dart';
 import 'package:users/screens/bloc/home_event.dart';
 import 'package:users/screens/bloc/home_state.dart';
 
-class HomeBloc extends Bloc<HomeEvent, HomeState>{}
+class HomeBloc extends Bloc<HomeEvent, HomeState>{
+
+  final UserRepository userRepository;
+
+  HomeBloc(this.userRepository):super(HomeLoadingState()){
+    on<HomeEvent>((event, emit) async{
+      final users = await userRepository.getUsers();
+      emit(HomeLoadedState(users));
+    });
+  }
+}
--- lib/screens/view/home_view.dart
+++ lib/screens/view/home_view.dart
@@ -21,6 +21,25 @@ class HomeView extends StatelessWidget {
   }
 
   Scaffold scaffoldMethod(BuildContext context) => Scaffold(
-    
+    body: BlocConsumer<HomeBloc, HomeState>(
+      listener: (context, state) {
+        
+      },
+      builder: (context, state){
+        if(state is HomeLoadingState){
+          return Center(child: CircularProgressIndicator.adaptive(),);
+        } if (state is HomeErrorState) {
+          return Text("xato");
+        } else if(state is HomeLoadedState) {
+          return ListView.builder(
+            itemCount: state.props.length,
+            itemBuilder: (context, i){
+            return Text("${state.props[0].name == state.props[0].name}");
+          });
+        } else{
+          return Container();
+        }
+      },
+      ),
   );
 }
